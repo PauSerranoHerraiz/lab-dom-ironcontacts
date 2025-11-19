8eb94708d091a7e73e3b7935e7a95726d3dd3471
Pau Serrano <pauserrano14@gmail.com>
2025-11-19 15:12:32 +0100

    ironcontacts Lab Pau Serrano

--- src/index.js
+++ src/index.js
@@ -34,11 +34,49 @@ tableBody.appendChild(exampleRow);
 
 
 
+
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
+
+
+threeContacts.forEach (contact => {
+
+const row = document.createElement("tr");
+row.innerHTML = `
+  <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+
+const deleteBtn = row.querySelector(".btn-delete")
+
+deleteBtn.addEventListener("click", () => {
+  row.remove()
+})
+
+const likeBtn = row.querySelector(".btn-like")
+
+likeBtn.addEventListener("click", () => {
+  likeBtn.classList.toggle("selected")
+})
+
+tableBody.appendChild(row);
+});
+
+
 
 
   
